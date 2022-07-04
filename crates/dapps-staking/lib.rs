3509a2fe556f7ca7bb84951229aba622fb842c40
PierreOssun <pierre.giraud@mailfence.com>
2022-07-04 17:18:37 +0200

    Added example folder + contracts

--- crates/dapps-staking/lib.rs
+++ crates/dapps-staking/lib.rs
@@ -34,6 +34,15 @@ impl DappsStaking {
             .call(&())
     }
 
+    /// Calls general_era_info() in the pallet-dapps-staking
+    pub fn read_era_info(era: u32) -> Result<EraInfo<Balance>, DSError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(00u32)
+            .input::<u32>()
+            .output::<Result<EraInfo<Balance>, DSError>>()
+            .handle_error_code::<DSError>()
+            .call(&era)?
+    }
+
     /// Fetch reward from EraRewardsAndStakes storage map
     /// Returns '0' if no rewards
     pub fn read_era_reward(era: u32) -> Result<Balance, DSError> {
@@ -163,15 +172,6 @@ impl DappsStaking {
             .handle_error_code::<DSError>()
             .call(&input)?
     }
-
-    /// Calls general_era_info() in the pallet-dapps-staking
-    pub fn read_era_info(era: u32) -> Result<EraInfo<Balance>, DSError> {
-        ::ink_env::chain_extension::ChainExtensionMethod::build(00u32)
-            .input::<u32>()
-            .output::<Result<EraInfo<Balance>, DSError>>()
-            .handle_error_code::<DSError>()
-            .call(&era)?
-    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Encode, Decode)]
@@ -209,14 +209,66 @@ pub struct EraInfo<Balance: HasCompact> {
 #[derive(scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum DSError {
-    Failed,
+    /// Disabled
+    Disabled,
+    /// No change in maintenance mode
+    NoMaintenanceModeChange,
+    /// Upgrade is too heavy, reduce the weight parameter.
+    UpgradeTooHeavy,
+    /// Can not stake with zero value.
+    StakingWithNoValue,
+    /// Can not stake with value less than minimum staking value
+    InsufficientValue,
+    /// Number of stakers per contract exceeded.
+    MaxNumberOfStakersExceeded,
+    /// Targets must be operated contracts
+    NotOperatedContract,
+    /// Contract isn't staked.
+    NotStakedContract,
+    /// Contract isn't unregistered.
+    NotUnregisteredContract,
+    /// Unclaimed rewards should be claimed before withdrawing stake.
+    UnclaimedRewardsRemaining,
+    /// Unstaking a contract with zero value
+    UnstakingWithNoValue,
+    /// There are no previously unbonded funds that can be unstaked and withdrawn.
+    NothingToWithdraw,
+    /// The contract is already registered by other account
+    AlreadyRegisteredContract,
+    /// User attempts to register with address which is not contract
+    ContractIsNotValid,
+    /// This account was already used to register contract
+    AlreadyUsedDeveloperAccount,
+    /// Smart contract not owned by the account id.
+    NotOwnedContract,
+    /// Report issue on github if this is ever emitted
+    UnknownEraReward,
+    /// Report issue on github if this is ever emitted
+    UnexpectedStakeInfoEra,
+    /// Contract has too many unlocking chunks. Withdraw the existing chunks if possible
+    /// or wait for current chunks to complete unlocking process to withdraw them.
+    TooManyUnlockingChunks,
+    /// Contract already claimed in this era and reward is distributed
+    AlreadyClaimedInThisEra,
+    /// Era parameter is out of bounds
+    EraOutOfBounds,
+    /// Too many active `EraStake` values for (staker, contract) pairing.
+    /// Claim existing rewards to fix this problem.
+    TooManyEraStakeValues,
+    /// To register a contract, pre-approval is needed for this address
+    RequiredContractPreApproval,
+    /// Developer's account is already part of pre-approved list
+    AlreadyPreApprovedDeveloper,
+    /// Account is not actively staking
+    NotActiveStaker,
+    /// Transfering nomination to the same contract
+    NominationTransferToSameContract,
 }
 
 impl ink_env::chain_extension::FromStatusCode for DSError {
     fn from_status_code(status_code: u32) -> Result<(), Self> {
         match status_code {
             0 => Ok(()),
-            1 => Err(Self::Failed),
             _ => panic!("encountered unknown status code"),
         }
     }
---
+++ examples/dapps-staking/lib.rs
@@ -0,0 +1,109 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod staking_example {
+    use dapps_staking_extension::*;
+
+    #[derive(scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum StakingError {
+        DsError(DSError),
+    }
+
+    #[ink(storage)]
+    pub struct Staking {}
+
+    impl Staking {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message)]
+        pub fn read_current_era(&self) -> u32 {
+            DappsStaking::read_current_era()
+        }
+
+        #[ink(message)]
+        pub fn read_unbonding_period(&self) -> u32 {
+            DappsStaking::read_unbonding_period()
+        }
+
+        #[ink(message)]
+        pub fn read_era_info(&self, era: u32) -> Result<EraInfo<Balance>, StakingError> {
+            DappsStaking::read_era_info(era).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn read_era_reward(&self, era: u32) -> Result<Balance, StakingError> {
+            DappsStaking::read_era_reward(era).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn read_era_staked(&self, era: u32) -> Result<Balance, StakingError> {
+            DappsStaking::read_era_staked(era).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn read_staked_amount(&self, account: AccountId) -> Result<Balance, StakingError> {
+            DappsStaking::read_staked_amount(account).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn read_staked_amount_on_contract(&self, account: AccountId) -> Result<Balance, StakingError> {
+            DappsStaking::read_staked_amount_on_contract(account).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn read_contract_stake(&self, account: AccountId) -> Result<Balance, StakingError> {
+            DappsStaking::read_contract_stake(account).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn register(&mut self, contract: AccountId) -> Result<(), StakingError> {
+            DappsStaking::register(contract).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn bond_and_stake(&mut self, contract: AccountId, value: Balance) -> Result<(), StakingError> {
+            DappsStaking::bond_and_stake(contract, value).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn unbond_and_stake(&mut self, contract: AccountId, value: Balance) -> Result<(), StakingError> {
+            DappsStaking::unbond_and_stake(contract, value).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn withdraw_unbonded(&mut self) -> Result<(), StakingError> {
+            DappsStaking::withdraw_unbonded().map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn claim_dapp(&mut self, account_id: AccountId, value: Balance) -> Result<(), StakingError> {
+            DappsStaking::claim_dapp(account_id, value).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn claim_staker(&mut self, account_id: AccountId) -> Result<(), StakingError> {
+            DappsStaking::claim_staker(account_id).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn set_reward_destination(&mut self, destination: u8) -> Result<(), StakingError> {
+            DappsStaking::set_reward_destination(destination).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn withdraw_from_unregistered(&mut self, account_id: AccountId) -> Result<(), StakingError> {
+            DappsStaking::withdraw_from_unregistered(account_id).map_err(|e| return StakingError::DsError(e))
+        }
+
+        #[ink(message)]
+        pub fn nomination_transfer(&mut self, account_id: AccountId, value: Balance) -> Result<(), StakingError> {
+            DappsStaking::nomination_transfer(account_id, value).map_err(|e| return StakingError::DsError(e))
+        }
+    }
+}
