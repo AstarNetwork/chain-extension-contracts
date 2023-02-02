17f015af7822edde98dd79663239772fa0becedf
PierreOssun <35110271+PierreOssun@users.noreply.github.com>
2023-02-02 17:47:20 +0100

    Added tests for assets chain extension (#11)
    * Added AssetsExtension trait
    
    * Added asset mock
    
    * Added remaining tests
    
    * PR fixs

---
+++ crates/assets/lib.rs
@@ -0,0 +1,238 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_storage::traits::{
+    PackedLayout,
+    SpreadLayout,
+    StorageLayout,
+};
+
+use ink_env::{
+    AccountId,
+    DefaultEnvironment,
+    Environment,
+};
+use ink_prelude::vec::Vec;
+type Balance = <DefaultEnvironment as Environment>::Balance;
+
+pub struct AssetsExtension;
+
+impl AssetsExtension {
+    pub fn create(origin: Origin, id: u128, admin: AccountId, min_balance: Balance) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60001)
+            .input::<(Origin, u128, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, admin, min_balance))
+    }
+
+    pub fn transfer(origin: Origin, id: u128, target: AccountId, min_balance: Balance) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60002)
+            .input::<(Origin, u128, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, target, min_balance))
+    }
+
+    pub fn mint(origin: Origin, id: u128, beneficiary: AccountId, amount: Balance) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60003)
+            .input::<(Origin, u128, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, beneficiary, amount))
+    }
+
+    pub fn burn(origin: Origin, id: u128, who: AccountId, amount: Balance) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60004)
+            .input::<(Origin, u128, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, who, amount))
+    }
+
+    pub fn balance_of(id: u128, who: AccountId) -> Balance {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60005)
+            .input::<(u128, AccountId)>()
+            .output::<Balance>()
+            .ignore_error_code()
+            .call(&(id, who))
+    }
+
+    pub fn total_supply(id: u128) -> Balance {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60006)
+            .input::<u128>()
+            .output::<Balance>()
+            .ignore_error_code()
+            .call(&id)
+    }
+
+    pub fn allowance(id: u128, owner: AccountId, delegate: AccountId) -> Balance {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60007)
+            .input::<(u128, AccountId, AccountId)>()
+            .output::<Balance>()
+            .ignore_error_code()
+            .call(&(id, owner, delegate))
+    }
+
+    pub fn approve_transfer(origin: Origin, id: u128, delegate: AccountId, amount: Balance) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60008)
+            .input::<(Origin, u128, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, delegate, amount))
+    }
+
+    pub fn cancel_approval(origin: Origin, id: u128, delegate: AccountId) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F60009)
+            .input::<(Origin, u128, AccountId)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, delegate))
+    }
+
+    pub fn transfer_approved(
+        origin: Origin,
+        id: u128,
+        owner: AccountId,
+        destination: AccountId,
+        amount: Balance,
+    ) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F6000A)
+            .input::<(Origin, u128, AccountId, AccountId, Balance)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, owner, destination, amount))
+    }
+
+    pub fn set_metadata(
+        origin: Origin,
+        id: u128,
+        name: Vec<u8>,
+        symbol: Vec<u8>,
+        decimals: u8,
+    ) -> Result<(), AssetsError> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F6000B)
+            .input::<(Origin, u128, Vec<u8>, Vec<u8>, u8)>()
+            .output::<()>()
+            .handle_error_code::<AssetsError>()
+            .call(&(origin, id, name, symbol, decimals))
+    }
+
+    pub fn metadata_name(id: u128) -> Vec<u8> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F6000C)
+            .input::<u128>()
+            .output::<Vec<u8>>()
+            .ignore_error_code()
+            .call(&id)
+    }
+
+    pub fn metadata_symbol(id: u128) -> Vec<u8> {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F6000D)
+            .input::<u128>()
+            .output::<Vec<u8>>()
+            .ignore_error_code()
+            .call(&id)
+    }
+
+    pub fn metadata_decimals(id: u128) -> u8 {
+        ::ink_env::chain_extension::ChainExtensionMethod::build(0x48F6000E)
+            .input::<u128>()
+            .output::<u8>()
+            .ignore_error_code()
+            .call(&id)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
+pub enum Origin {
+    Caller,
+    Address,
+}
+
+impl Default for Origin {
+    fn default() -> Self {
+        Self::Address
+    }
+}
+
+impl ink_storage::traits::SpreadAllocate for Origin {
+    fn allocate_spread(_ptr: &mut ink_primitives::KeyPtr) -> Self {
+        Self::Address
+    }
+}
+
+#[derive(PartialEq, Eq, Copy, Clone, scale::Encode, scale::Decode, Debug)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum AssetsError {
+    /// Success
+    Success = 0,
+    /// Account balance must be greater than or equal to the transfer amount.
+    BalanceLow = 1,
+    /// The account to alter does not exist.
+    NoAccount = 2,
+    /// The signing account has no permission to do the operation.
+    NoPermission = 3,
+    /// The given asset ID is unknown.
+    Unknown = 4,
+    /// The origin account is frozen.
+    Frozen = 5,
+    /// The asset ID is already taken.
+    InUse = 6,
+    /// Invalid witness data given.
+    BadWitness = 7,
+    /// Minimum balance should be non-zero.
+    MinBalanceZero = 8,
+    /// Unable to increment the consumer reference counters on the account. Either no provider
+    /// reference exists to allow a non-zero balance of a non-self-sufficient asset, or the
+    /// maximum number of consumers has been reached.
+    NoProvider = 9,
+    /// Invalid metadata given.
+    BadMetadata = 10,
+    /// No approval exists that would allow the transfer.
+    Unapproved = 11,
+    /// The source account would not survive the transfer and it needs to stay alive.
+    WouldDie = 12,
+    /// The asset-account already exists.
+    AlreadyExists = 13,
+    /// The asset-account doesn't have an associated deposit.
+    NoDeposit = 14,
+    /// The operation would result in funds being burned.
+    WouldBurn = 15,
+    /// Unknown error
+    RuntimeError = 99,
+    /// Encountered unknown status code
+    UnknownStatusCode,
+    /// Encountered unexpected invalid SCALE encoding
+    InvalidScaleEncoding,
+}
+
+impl ink_env::chain_extension::FromStatusCode for AssetsError {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::BalanceLow),
+            2 => Err(Self::NoAccount),
+            3 => Err(Self::NoPermission),
+            4 => Err(Self::Unknown),
+            5 => Err(Self::Frozen),
+            6 => Err(Self::InUse),
+            7 => Err(Self::BadWitness),
+            8 => Err(Self::MinBalanceZero),
+            9 => Err(Self::NoProvider),
+            10 => Err(Self::BadMetadata),
+            11 => Err(Self::Unapproved),
+            12 => Err(Self::WouldDie),
+            13 => Err(Self::AlreadyExists),
+            14 => Err(Self::NoDeposit),
+            15 => Err(Self::WouldBurn),
+            99 => Err(Self::RuntimeError),
+            _ => Err(Self::UnknownStatusCode),
+        }
+    }
+}
+
+impl From<scale::Error> for AssetsError {
+    fn from(_: scale::Error) -> Self {
+        AssetsError::InvalidScaleEncoding
+    }
+}
---
+++ examples/assets/lib.rs
@@ -0,0 +1,118 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod contract {
+    use assets_extension::*;
+    use ink_prelude::vec::Vec;
+    use ink_storage::traits::SpreadAllocate;
+
+    #[ink(storage)]
+    #[derive(Default, SpreadAllocate)]
+    pub struct Mock {}
+
+    impl Mock {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[ink(message, payable)]
+        pub fn create(&mut self, asset_id: u128, min_balance: Balance) -> Result<(), AssetsError> {
+            let caller = self.env().caller();
+            AssetsExtension::create(Origin::Caller, asset_id, caller, min_balance)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn mint(&mut self, asset_id: u128, beneficiary: AccountId, amount: Balance) -> Result<(), AssetsError> {
+            AssetsExtension::mint(Origin::Caller, asset_id, beneficiary, amount)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn burn(&mut self, asset_id: u128, who: AccountId, amount: Balance) -> Result<(), AssetsError> {
+            AssetsExtension::burn(Origin::Caller, asset_id, who, amount)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, asset_id: u128, target: AccountId, amount: Balance) -> Result<(), AssetsError> {
+            AssetsExtension::transfer(Origin::Caller, asset_id, target, amount)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, asset_id: u128, who: AccountId) -> Balance {
+            AssetsExtension::balance_of(asset_id, who)
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self, asset_id: u128) -> Balance {
+            AssetsExtension::total_supply(asset_id)
+        }
+
+        #[ink(message)]
+        pub fn allowance(&self, asset_id: u128, owner: AccountId, delegate: AccountId) -> Balance {
+            AssetsExtension::allowance(asset_id, owner, delegate)
+        }
+
+        #[ink(message)]
+        pub fn approve_transfer(
+            &mut self,
+            asset_id: u128,
+            delegate: AccountId,
+            amount: Balance,
+        ) -> Result<(), AssetsError> {
+            AssetsExtension::approve_transfer(Origin::Caller, asset_id, delegate, amount)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn cancel_approval(&mut self, asset_id: u128, delegate: AccountId) -> Result<(), AssetsError> {
+            AssetsExtension::cancel_approval(Origin::Caller, asset_id, delegate)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn transfer_approved(
+            &mut self,
+            asset_id: u128,
+            owner: AccountId,
+            destination: AccountId,
+            amount: Balance,
+        ) -> Result<(), AssetsError> {
+            AssetsExtension::transfer_approved(Origin::Caller, asset_id, owner, destination, amount)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn set_metadata(
+            &mut self,
+            asset_id: u128,
+            name: Vec<u8>,
+            symbol: Vec<u8>,
+            decimals: u8,
+        ) -> Result<(), AssetsError> {
+            AssetsExtension::set_metadata(Origin::Caller, asset_id, name, symbol, decimals)?;
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn metadata_name(&self, asset_id: u128) -> Vec<u8> {
+            AssetsExtension::metadata_name(asset_id)
+        }
+
+        #[ink(message)]
+        pub fn metadata_symbol(&self, asset_id: u128) -> Vec<u8> {
+            AssetsExtension::metadata_symbol(asset_id)
+        }
+
+        #[ink(message)]
+        pub fn metadata_decimals(&self, asset_id: u128) -> u8 {
+            AssetsExtension::metadata_decimals(asset_id)
+        }
+    }
+}
